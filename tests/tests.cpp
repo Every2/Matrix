3a13d6d1db05587c335f431d20583185ed2ddb49
Every2 <christiansantoslima21@gmail.com>
2024-06-03 12:00:52 -0300

    fix: missing namespace in tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,83 +1,84 @@
 #include <gtest/gtest.h>
 #include "../include/specialized_functions.h"
 
+
 TEST(SumTest, MatrixSum) {
-    MatrixType<Rational, 3, 2> matrix {{
-        {{ Rational(2), Rational(3) }},
-        {{ Rational(8), Rational(1) }},
-        {{ Rational(-4), Rational(0) }}
+    mtx_f::MatrixType<mtx_f::Rational, 3, 2> matrix {{
+        {{ mtx_f::Rational(2), mtx_f::Rational(3) }},
+        {{ mtx_f::Rational(8), mtx_f::Rational(1) }},
+        {{ mtx_f::Rational(-4), mtx_f::Rational(0) }}
     }};
 
-    MatrixType<Rational, 3, 2> other {{
-        {{ Rational(4), Rational(5) }},
-        {{ Rational(-9), Rational(6) }},
-        {{ Rational(2), Rational(7) }}
+    mtx_f::MatrixType<mtx_f::Rational, 3, 2> other {{
+        {{ mtx_f::Rational(4), mtx_f::Rational(5) }},
+        {{ mtx_f::Rational(-9), mtx_f::Rational(6) }},
+        {{ mtx_f::Rational(2), mtx_f::Rational(7) }}
     }};
 
-    auto result = sum(matrix, other);
+    auto result = mtx_f::sum(matrix, other);
 
-    ASSERT_EQ(result[0][0], Rational(6));
-    ASSERT_EQ(result[0][1], Rational(8));
-    ASSERT_EQ(result[1][0], Rational(-1));
-    ASSERT_EQ(result[1][1], Rational(7));
-    ASSERT_EQ(result[2][0], Rational(-2));
-    ASSERT_EQ(result[2][1], Rational(7));
+    ASSERT_EQ(result[0][0], mtx_f::Rational(6));
+    ASSERT_EQ(result[0][1], mtx_f::Rational(8));
+    ASSERT_EQ(result[1][0], mtx_f::Rational(-1));
+    ASSERT_EQ(result[1][1], mtx_f::Rational(7));
+    ASSERT_EQ(result[2][0], mtx_f::Rational(-2));
+    ASSERT_EQ(result[2][1], mtx_f::Rational(7));
 }
 
 TEST(RowReductionTest, MatrixRowReduction) {
-    MatrixType<Rational, 4, 4> matrix {{
+    mtx_f::MatrixType<mtx_f::Rational, 4, 4> matrix {{
         {{
-            Rational(1, 1),
-            Rational(1, 1),
-            Rational(1, 1),
-            Rational(6, 1),
+            mtx_f::Rational(1, 1),
+            mtx_f::Rational(1, 1),
+            mtx_f::Rational(1, 1),
+            mtx_f::Rational(6, 1),
         }},
         {{
-            Rational(2, 1),
-            Rational(-1, 1),
-            Rational(3, 1),
-            Rational(11, 1),
+            mtx_f::Rational(2, 1),
+            mtx_f::Rational(-1, 1),
+            mtx_f::Rational(3, 1),
+            mtx_f::Rational(11, 1),
         }},
         {{
-            Rational(4, 1),
-            Rational(-3, 1),
-            Rational(2, 1),
-            Rational(0, 1),
+           mtx_f::Rational(4, 1),
+            mtx_f::Rational(-3, 1),
+            mtx_f::Rational(2, 1),
+           mtx_f::Rational(0, 1),
         }},
         {{
-            Rational(3, 1),
-            Rational(1, 1),
-            Rational(1, 1),
-            Rational(4, 1),
+            mtx_f::Rational(3, 1),
+            mtx_f::Rational(1, 1),
+            mtx_f::Rational(1, 1),
+            mtx_f::Rational(4, 1),
         }}
     }};
 
-    auto result = row_reduction(matrix);
+    auto result = mtx_f::row_reduction(matrix);
 
-    MatrixType<Rational, 4, 4> expected_result = {{
+    mtx_f::MatrixType<mtx_f::Rational, 4, 4> expected_result = {{
         {{
-            Rational(1, 1),
-            Rational(0, 1),
-            Rational(0, 1),
-            Rational(-1, 1),
+            mtx_f::Rational(1, 1),
+            mtx_f::Rational(0, 1),
+            mtx_f::Rational(0, 1),
+            mtx_f::Rational(-1, 1),
         }},
         {{
-            Rational(0, 1),
-            Rational(1, 1),
-            Rational(0, 1),
-            Rational(2, 1),
+            mtx_f::Rational(0, 1),
+            mtx_f::Rational(1, 1),
+            mtx_f::Rational(0, 1),
+            mtx_f::Rational(2, 1),
         }},
         {{
-            Rational(0, 1),
-            Rational(0, 1),
-            Rational(1, 1),
-            Rational(5, 1),
+           mtx_f::Rational(0, 1),
+            mtx_f::Rational(0, 1),
+            mtx_f::Rational(1, 1),
+            mtx_f::Rational(5, 1),
         }},
         {{
-            Rational(0, 1),
-            Rational(0, 1),
-            Rational(0, 1),
-            Rational(0, 1),
+            mtx_f::Rational(0, 1),
+            mtx_f::Rational(0, 1),
+            mtx_f::Rational(0, 1),
+            mtx_f::Rational(0, 1),
         }}
     }};
 
@@ -85,143 +86,143 @@ TEST(RowReductionTest, MatrixRowReduction) {
 }
 
 TEST(ScalarSubTest, ScalarSubtraction) {
-    MatrixType<Rational, 2, 3> matrix {{
+    mtx_f::MatrixType<mtx_f::Rational, 2, 3> matrix {{
         {{
-            Rational(2, 1),
-            Rational(3, 1),
-            Rational(8, 1),
+            mtx_f::Rational(2, 1),
+            mtx_f::Rational(3, 1),
+            mtx_f::Rational(8, 1),
         }},
         {{
-            Rational(1, 1),
-            Rational(-4, 1),
-            Rational(0, 1),
-        }},
-    }};
-
-    MatrixType<Rational, 2, 3> other = {{
-        {{
-            Rational(4, 1),
-            Rational(5, 1),
-            Rational(-9, 1),
-        }},
-        {{
-            Rational(6, 1),
-            Rational(2, 1),
-            Rational(7, 1),
+            mtx_f::Rational(1, 1),
+            mtx_f::Rational(-4, 1),
+            mtx_f::Rational(0, 1),
         }},
     }};
 
-    MatrixType<Rational, 2, 3> scalar_result = {{
+    mtx_f::MatrixType<mtx_f::Rational, 2, 3> other = {{
         {{
-            Rational(0, 1),
-            Rational(1, 1),
-            Rational(25, 1),
+            mtx_f::Rational(4, 1),
+            mtx_f::Rational(5, 1),
+            mtx_f::Rational(-9, 1),
         }},
         {{
-            Rational(-4, 1),
-            Rational(-10, 1),
-            Rational(-7, 1),
+            mtx_f::Rational(6, 1),
+            mtx_f::Rational(2, 1),
+            mtx_f::Rational(7, 1),
         }},
     }};
 
-    auto scalar_result_actual = scalar(matrix, 2);
+    mtx_f::MatrixType<mtx_f::Rational, 2, 3> scalar_result = {{
+        {{
+            mtx_f::Rational(0, 1),
+            mtx_f::Rational(1, 1),
+            mtx_f::Rational(25, 1),
+        }},
+        {{
+            mtx_f::Rational(-4, 1),
+            mtx_f::Rational(-10, 1),
+            mtx_f::Rational(-7, 1),
+        }},
+    }};
 
-    auto result = sub(scalar_result_actual, other);
+    auto scalar_result_actual = mtx_f::scalar(matrix, 2);
+
+    auto result = mtx_f::sub(scalar_result_actual, other);
 
     ASSERT_EQ(result, scalar_result);
 }
 
 TEST(DeterminantTest, MatrixDeterminant) {
-    MatrixType<Rational, 2, 2> matrix {{
+    mtx_f::MatrixType<mtx_f::Rational, 2, 2> matrix {{
         {{
-            Rational(1, 1),
-            Rational(2, 1),
+            mtx_f::Rational(1, 1),
+            mtx_f::Rational(2, 1),
         }},
         {{
-            Rational(3, 1),
-            Rational(4, 1),
+            mtx_f::Rational(3, 1),
+            mtx_f::Rational(4, 1),
         }},
     }};
 
-    auto actual_result = determinant(matrix);
+    auto actual_result = mtx_f::determinant(matrix);
 
     ASSERT_EQ(actual_result, -2);
 }
 
 TEST(TransposeTest, MatrixTranspose) {
-    MatrixType<Rational, 3, 2> matrix {{
+    mtx_f::MatrixType<mtx_f::Rational, 3, 2> matrix {{
         {{
-            Rational(2, 1),
-            Rational(0, 1),
+            mtx_f::Rational(2, 1),
+            mtx_f::Rational(0, 1),
         }},
         {{
-            Rational(8, 1),
-            Rational(6, 1),
+            mtx_f::Rational(8, 1),
+            mtx_f::Rational(6, 1),
         }},
         {{
-            Rational(-4, 1),
-            Rational(-10, 1),
+            mtx_f::Rational(-4, 1),
+            mtx_f::Rational(-10, 1),
         }},
     }};
 
-    auto result = transpose(matrix);
+    auto result = mtx_f::transpose(matrix);
 
-    MatrixType<Rational, 2, 3> expected_result {{
+    mtx_f::MatrixType<mtx_f::Rational, 2, 3> expected_result {{
         {{
-            Rational(2, 1),
-            Rational(8, 1),
-            Rational(-4, 1),
+            mtx_f::Rational(2, 1),
+            mtx_f::Rational(8, 1),
+            mtx_f::Rational(-4, 1),
         }},
         {{
-            Rational(0, 1),
-            Rational(6, 1),
-            Rational(-10, 1),
+            mtx_f::Rational(0, 1),
+            mtx_f::Rational(6, 1),
+            mtx_f::Rational(-10, 1),
         }},
     }};
     ASSERT_EQ(result, expected_result);
 }
 
 TEST(FormulaMultiTest, MatrixFormulaMulti) {
-    MatrixType<Rational, 2, 3> matrix = {{
+    mtx_f::MatrixType<mtx_f::Rational, 2, 3> matrix = {{
         {{
-            Rational(1, 1),
-            Rational(2, 1),
-            Rational(3, 1),
+            mtx_f::Rational(1, 1),
+            mtx_f::Rational(2, 1),
+            mtx_f::Rational(3, 1),
         }},
         {{
-            Rational(0, 1),
-            Rational(6, 1),
-            Rational(1, 1),
-        }},
-    }};
-
-    MatrixType<Rational, 3, 2> another = {{
-        {{
-            Rational(1, 1),
-            Rational(1, 1),
-        }},
-        {{
-            Rational(4, 1),
-            Rational(4, 1),
-        }},
-        {{
-            Rational(2, 1),
-            Rational(2, 1),
+            mtx_f::Rational(0, 1),
+            mtx_f::Rational(6, 1),
+            mtx_f::Rational(1, 1),
         }},
     }};
 
-    MatrixType<Rational, 2, 2> result = {{
+    mtx_f::MatrixType<mtx_f::Rational, 3, 2> another = {{
         {{
-            Rational(15, 1),
-            Rational(15, 1),
+            mtx_f::Rational(1, 1),
+            mtx_f::Rational(1, 1),
         }},
         {{
-            Rational(26, 1),
-            Rational(26, 1),
+            mtx_f::Rational(4, 1),
+            mtx_f::Rational(4, 1),
+        }},
+        {{
+            mtx_f::Rational(2, 1),
+            mtx_f::Rational(2, 1),
         }},
     }};
 
-    auto actual_result = product(matrix, another);
+    mtx_f::MatrixType<mtx_f::Rational, 2, 2> result = {{
+        {{
+            mtx_f::Rational(15, 1),
+            mtx_f::Rational(15, 1),
+        }},
+        {{
+            mtx_f::Rational(26, 1),
+            mtx_f::Rational(26, 1),
+        }},
+    }};
+
+    auto actual_result = mtx_f::product(matrix, another);
 
     ASSERT_EQ(actual_result, result);
 }
